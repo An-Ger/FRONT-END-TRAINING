bb5038b91e4239387916e5dcf85f68376818d8a1
an-ger <2455927306@qq.com>
2020-07-23 14:15:44 +0800

    毛玻璃

---
+++ TypeScript/demo_02.ts
@@ -0,0 +1,15 @@
+for(var i = 0; i < 10; i++) {
+    setTimeout(function(){
+        console.log(i);
+        
+    }, 100 * i)
+}
+// 10 10 10 10 10 10 10 10 10 10
+for (var i = 0; i < 10; i++) {
+    (function(i){
+        setTimeout(function(){
+            console.log(i);
+            
+        }, 100 * i)
+    })
+}
